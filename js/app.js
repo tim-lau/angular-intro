9fddda0397503f0b05be90d818cfae33bf87a153
Tim Lau <timothyl@uw.edu>
2014-11-04 09:54:50 -0800

    displayed array of movies with angularJS

--- js/app.js
+++ js/app.js
@@ -5,3 +5,9 @@
 
 "use strict";
 
+angular.module('MoviesApp', [])
+	.controller('MoviesController', function($scope) {
+		//intialize variables with list of movies so that it'll be accessed in the view
+		$scope.movies = movies;
+
+	});
